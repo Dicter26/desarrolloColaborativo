260dd968986631b35b654f3b31c276e6fd417e34
Raul Llamosas <raul.llamosas182@gmail.com>
2023-03-06 17:17:51 -0600

    Adding user comment.
    I added my comment to the first line as required by the assignment.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Authors: Dicter Tadeo Garcia Rosas - dicter.r26@ciencias.unam.mx, Aldo Iván García Salman - salman@ciencias.unam.mx
+// Authors: Dicter Tadeo Garcia Rosas - dicter.r26@ciencias.unam.mx, Aldo Iván García Salman - salman@ciencias.unam.mx , Raul Llamosas Alvarado - raul.llamosa182@gmail.com
 const http = required('http')
 const server = http.createServer((peticion, respuesta) => {
     respuesta.writeHead(200, {'Content-Type': 'text/plain'})
